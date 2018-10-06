27c337d53add7fc0138499388f8e9d8b8306c1c3
Ipsedo <7268@samu>
2018-10-06 19:30:22 +0200

    reformat code + add other operator override

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,48 @@
 #include "utils.h"
 
 int main(int argc, char **argv) {
-  vector<vector<float>> v1;
-  vector<vector<float>> v2;
+	vector<vector<float>> v1;
+	vector<vector<float>> v2;
 
-  int size = 100;
+	int size = 10;
 
-  for (int i = 0; i < size; i++) {
-    vector<float> tmp1;
-    vector<float> tmp2;
+	for (int i = 0; i < size; i++) {
+		vector<float> tmp1;
+		vector<float> tmp2;
 
-    for (int j = 0; j < size; j++) {
-      tmp1.push_back(2.f);
-      tmp2.push_back(i * size + j);
-    }
+		for (int j = 0; j < size; j++) {
+			tmp1.push_back(2.f);
+			tmp2.push_back(i * size + j);
+		}
 
-    v1.push_back(tmp1);
-    v2.push_back(tmp2);
-  }
+		v1.push_back(tmp1);
+		v2.push_back(tmp2);
+	}
 
-  vector<vector<float>> res = v1 * v2;
+	vector<vector<float>> res = v1 * v2;
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++)
+			cout << res[i][j] << ", ";
+		cout << endl;
+	}
+	cout << endl;
 
-  for (int i = 0; i < res.size(); i++) {
-    for (int j = 0; j < res[i].size(); j++)
-      cout << res[i][j] << ", ";
-    cout << endl;
-  }
+	res = v1 * 10.f;
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++)
+			cout << res[i][j] << ", ";
+		cout << endl;
+	}
+	cout << endl;
 
-
-  return 0;
+	vector<float> v3;
+	for (int i = 0; i < size; i++)
+		v3.push_back(i + 1.f);
+	res = v2 * v3;
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++)
+			cout << res[i][j] << ", ";
+		cout << endl;
+	}
+	return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -3,30 +3,37 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 struct individual {
-  vector<double> x;
-  vector<double> s;
-  double f_value;
+	vector<double> x;
+	vector<double> s;
+	double f_value;
 };
 
 #define SIZE_ERROR 2
 
 template<typename T>
 vector<T> operator*(vector<T> v1, vector<T> v2) {
-  if (v1.size() != v2.size()) {
-    cout << "Size " << v1.size()
-      << " and " << v2.size()
-      << " are incompatible" << endl;
-    exit(SIZE_ERROR);
-  }
-  vector<T> res;
-  for (int i = 0; i < v1.size(); i++) {
-    res.push_back(v1[i] * v2[i]);
-  }
-  return res;
+	if (v1.size() != v2.size()) {
+		cout << "Size " << v1.size()
+			 << " and " << v2.size()
+			 << " are incompatible" << endl;
+		exit(SIZE_ERROR);
+	}
+	vector<T> res;
+	for (int i = 0; i < v1.size(); i++) {
+		res.push_back(v1[i] * v2[i]);
+	}
+	return res;
+}
+
+template<typename T, typename U>
+vector<T> operator*(vector<T> v, U s) {
+	transform(v.begin(), v.end(), v.begin(), [&s](T t) { return t * s; });
+	return v;
 }
 
 #endif //UTILS_H
