2fe75469b2bdc358d0977cf5533320651c32f55a
Ipsedo <7268@samu>
2018-10-08 00:40:08 +0200

    suppr trucs inutiles + use normal distribution

--- src/algo/SA_ES.cpp
+++ src/algo/SA_ES.cpp
@@ -5,18 +5,17 @@
 #include <algorithm>
 #include "SA_ES.h"
 
-#define SEED 123456
+#define NOISE 1e-1
 
 // tau ≈ 1 / sqrt(n) ? ajouter random ?
 SA_ES::SA_ES(coco_problem_s *p)
 		: problem(p), n(static_cast<int>(coco_problem_get_dimension(p))),
 		  d(static_cast<int>(coco_problem_get_number_of_objectives(p))),
-		  /*nbConstraint(static_cast<int>(coco_problem_get_number_of_constraints(p))),*/
 		  lambda(5 * n), mu(lambda / 4),
-		  tau(1. / sqrt(n)), taus(vector<double>((unsigned long) n)),
-		  parent(individual{vector<double>((unsigned long) n), vector<double>((unsigned long) n), 0.}){
-	generate(taus.begin(), taus.end(), [this](){ return 1. / pow(n, 1. / 4.); });
-	srand(SEED);
+		  tau(1. / sqrt(n) + NOISE * double(rand()) / double(RAND_MAX)), taus(vector<double>((unsigned long) n)),
+		  parent(individual{vector<double>((unsigned long) n, 0.), vector<double>((unsigned long) n, 0.), 0.}),
+			distribution(0., 1.), generator() {
+	generate(taus.begin(), taus.end(), [this](){ return 1. / pow(n, 1. / 4.) + NOISE * double(rand()) / double(RAND_MAX); });
 }
 
 void SA_ES::step() {
@@ -25,28 +24,16 @@ void SA_ES::step() {
 	for (int i = 0; i < lambda; i++) {
 		individual c {vector<double>((unsigned long) n), vector<double>((unsigned long) n), 0.};
 
-		double xi = tau *
-				getNormalValue(2. * double(rand()) / double(RAND_MAX) - 1.,
-										 0., 1.); // quelles bornes pour tirer un nb selon loi normale ?
-		vector<double> xis = taus * getNormalValues(n, 0., 1.);
-		vector<double> z = getNormalValues(n, 0., 1.);
+		double xi = tau * distribution(generator);
+		vector<double> xis = taus * getNormalValues(n, distribution, generator);
+		vector<double> z = getNormalValues(n, distribution, generator);
 
 		// mutation
 		transform(xis.begin(), xis.end(), xis.begin(), [](double d) { return exp(d); });
 		c.s = parent.s * xis * exp(xi);
 		c.x = parent.x + c.s * z;
 
-		//c.f_value = ?
-		vector<double> y((unsigned long) d);
-		coco_evaluate_function(problem, c.x.data(), y.data());
-		c.f_value = norm(y);
-
-		/*if (nbConstraint > 0) {
-			vector<double> cons((unsigned long) nbConstraint);
-			coco_evaluate_constraint(problem, c.x.data(), cons.data());
-			cons.clear();
-		}*/
-		y.clear();
+		coco_evaluate_function(problem, c.x.data(), &c.f_value);
 
 		childens.push_back(c);
 	}
@@ -54,13 +41,15 @@ void SA_ES::step() {
 	// Sort for select best
 	sort(childens.begin(), childens.end(),
 		 [](individual i1, individual i2){
-			 if (i1.f_value == i2.f_value) return 0;
-			 else if (i1.f_value > i2.f_value) return 1;
-			 else return -1;});
+			 if (i1.f_value > i2.f_value) return 1;
+			 else if (i1.f_value < i2.f_value) return -1;
+			 else return 0;});
 
 	// Recombination
-	parent.x = parent.x * 0.;
-	parent.s = parent.s * 0.;
+	for (int i = 0; i < n; i++) {
+		parent.x[i] = 0.;
+		parent.s[i] = 0.;
+	}
 	double invMu = (1. / double(mu));
 	for (int i = 0; i < mu; i++) {
 		parent.x = parent.x + childens[i].x * invMu;
