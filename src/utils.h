9eb7e6c63acae10ecc8f1a5c035dd74b15173f9f
Ipsedo <7268@samu>
2018-10-07 16:34:30 +0200

    add operator+ vector<T>, add get normal value fct

--- src/utils.h
+++ src/utils.h
@@ -36,4 +36,21 @@ vector<T> operator*(vector<T> v, U s) {
 	return v;
 }
 
+template<typename T>
+vector<T> operator+(vector<T> v1, vector<T> v2) {
+	if (v1.size() != v2.size()) {
+		cout << "Size " << v1.size()
+			 << " and " << v2.size()
+			 << " are incompatible" << endl;
+		exit(SIZE_ERROR);
+	}
+	vector<T> res;
+	for (int i = 0; i < v1.size(); i++) {
+		res.push_back(v1[i] + v2[i]);
+	}
+	return res;
+}
+
+double getNormalValue(double x, double mu, double sig);
+
 #endif //UTILS_H
